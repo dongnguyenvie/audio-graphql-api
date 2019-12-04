de41016007ffd50c0de5be44ba4c149f441c8928
Dong Nguyen <dong.nguyen@gmail.com>
2019-12-05 00:30:04 +0700

    refactor for ctrs.role

--- src/graphQLSchema/resolvers/mutations-queries/role.js
+++ src/graphQLSchema/resolvers/mutations-queries/role.js
@@ -2,7 +2,7 @@ import { AuthenticationError } from 'apollo-server'
 import ctrs from '../controller'
 import helper from '../../../core/common/helper'
 
-const FIELD_NM = 'role'
+const FIELD = 'role'
 /**
  * Role resolvers
  * async (root, args, context, info)
@@ -11,22 +11,22 @@ export default {
   Query: {
     getRole: async (_, args, { models, me }, info) => {
       const projection = helper.getProjection(info)
-      return ctrs[FIELD_NM].getRole(models[FIELD_NM], args[FIELD_NM], { projection })
+      return ctrs[FIELD].getRole(models[FIELD], args[FIELD], { projection })
     },
     getRoles: async (_, args, { models, me }, info) => {
       const projection = helper.getProjection(info)
-      return ctrs[FIELD_NM].getRoles(models[FIELD_NM], args[FIELD_NM], { projection })
+      return ctrs[FIELD].getRoles(models[FIELD], args[FIELD], { projection })
     }
   },
   Mutation: {
     createRole: async (_, args, { models }, info) => {
-      return ctrs[FIELD_NM].createRole(models[FIELD_NM], args[FIELD_NM])
+      return ctrs[FIELD].createRole(models[FIELD], args[FIELD])
     },
     updateRole: async (_, args, { models }, info) => {
-      return ctrs[FIELD_NM].updateRole(models[FIELD_NM], args[FIELD_NM])
+      return ctrs[FIELD].updateRole(models[FIELD], args[FIELD])
     },
     deleteRole: async (_, args, { models }, info) => {
-      return ctrs[FIELD_NM].deleteRole(models[FIELD_NM], args[FIELD_NM])
+      return ctrs[FIELD].deleteRole(models[FIELD], args[FIELD])
     }
   }
 }
